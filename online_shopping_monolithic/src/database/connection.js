86c5c76431a9b6fde49d453d529d5e73d9968de2
salma <sa1963645@gmail.com>
2025-04-06 13:56:17 +0200

    fix: resolve MongoDB connection issues

--- online_shopping_monolithic/src/database/connection.js
+++ online_shopping_monolithic/src/database/connection.js
@@ -1,22 +1,29 @@
 const mongoose = require('mongoose');
 const { DB_URL } = require('../config');
 
-module.exports = async() => {
+module.exports = async () => {
 
     try {
         await mongoose.connect(DB_URL, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
+            useUnifiedTopology: true
         });
-        console.log('Db Connected');
-        
+        console.log('DataBase connected');
+        try {
+            await mongoose.connect(DB_URL, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+            console.log('Db Connected');
+
+        } catch (error) {
+            console.log('Error ============')
+            console.log(error);
+            process.exit(1);
+        }
     } catch (error) {
         console.log('Error ============')
         console.log(error);
         process.exit(1);
     }
- 
 };
-
- 
